3f9101287a90b36b9c587127a1360968126e88a2
Nick Dyer <nbdyer@gmail.com>
2016-05-02 21:31:26 +0100

    Adds two more tests for welcome and greeting a user

--- exercise_one/lib/exercise_one.rb
+++ exercise_one/lib/exercise_one.rb
@@ -1,11 +1,22 @@
 class User_Interface 
 
-  def initialize(output)
+  def initialize(input = STDIN, output)
+    @input = input
     @output = output
   end
 
   def query_name
     @output.puts("What is your name?")
+  end
+
+  def greet_user(name)
+    @output.puts("Hello, #{name}, nice to meet you!")
+  end
+
+  def welcome_user
+    query_name
+    name = @input.gets.chomp
+    greet_user name
   end
 
 end
--- exercise_one/spec/exercise_one_spec.rb
+++ exercise_one/spec/exercise_one_spec.rb
@@ -10,4 +10,21 @@ RSpec.describe User_Interface do
     expect(output.string).to include "What is your name?"
   end
 
+  it "Can greet a named user" do
+    output = StringIO.new
+    user_interface = User_Interface.new(output)
+    user_interface.greet_user("Nick")
+    expect(output.string).to include "Hello, Nick, nice to meet you!"
+  end
+
+  it "Can take a users name and greet them" do
+    input = StringIO.new("Nick")
+    output = StringIO.new
+    user_interface = User_Interface.new(input, output)
+    user_interface.welcome_user
+    expect(output.string).to include "What is your name?"
+    expect(output.string).to include "Nick"
+    expect(output.string).to include "Hello, Nick, nice to meet you!"
+  end
+
 end
